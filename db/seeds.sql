5deea8200c8ed1e2d80a98bdc69a19370244dc71
Brianna Bullock <brianna.bullock16@gmail.com>
2020-08-03 21:41:07 -0500

    created seeds.sql ran in command line

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Krabby Patty');
+INSERT INTO burgers (burger_name) VALUES ('Mojja Bacon in Da Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken burger', true);
