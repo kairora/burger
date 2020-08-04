f7bef83f241d32f7fc3e8b411b2decf4d1071abc
Brianna Bullock <brianna.bullock16@gmail.com>
2020-08-04 00:00:13 -0500

    fixed first orm method name

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,7 @@
 const connection = require("../config/connection.js")
 
 var orm = {
-    selectAall: function(table, cb) {
+    selectAll: function(table, cb) {
       var query = "SELECT * FROM ??";
       connection.query(query, [table], function(err, result) {
         if (err) {
